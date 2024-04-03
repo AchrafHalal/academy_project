b625458c70a1e1975457b4c309b9044023e5199f
Othmansalahi <ootmansalhi.24@gmail.com>
2024-04-03 14:18:52 +0000

    Refactor departement index.blade.php form

--- resources/views/Departement/index.blade.php
+++ resources/views/Departement/index.blade.php
@@ -1,15 +1,16 @@
 @extends('Departement.layout')
 
 @section('navbar')
-    <h1>hello departement</h1>
-    <form id="departementForm" action="{{ route('departement.show', ['departement' => '__departement__']) }}" method="GET">
-        <select name="departement" id="departementSelect">
+    <h1 class="text-2xl font-bold mb-4">Hello Departement</h1>
+    <form id="departementForm" action="{{ route('departement.show', ['departement' => '__departement__']) }}" method="GET" class="flex items-center">
+        <select name="departement" id="departementSelect" class="px-4 py-2 border border-gray-300 rounded-md mr-2">
+                <option value="">--SELECT AN OPTION--</option>
             @foreach ($departements as $departement)
                 <option value="{{ $departement->departement }}">{{ $departement->departement }}</option>
             @endforeach
         </select>
         <input type="hidden" name="selected_departement" id="selected_departement">
-        <button type="submit">Submit</button>
+        <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">Submit</button>
     </form>
 
     <script>
