d97c3520089a6bfd2428d5b664aade32ad4c80c9
Othmansalahi <ootmansalhi.24@gmail.com>
2024-04-03 14:23:50 +0000

    Update Departement index.blade.php

--- resources/views/Departement/index.blade.php
+++ resources/views/Departement/index.blade.php
@@ -1,18 +1,19 @@
 @extends('Departement.layout')
 
 @section('navbar')
-    <h1 class="text-2xl font-bold mb-4">Hello Departement</h1>
-    <form id="departementForm" action="{{ route('departement.show', ['departement' => '__departement__']) }}" method="GET" class="flex items-center">
-        <select name="departement" id="departementSelect" class="px-4 py-2 border border-gray-300 rounded-md mr-2">
-                <option value="">--SELECT AN OPTION--</option>
-            @foreach ($departements as $departement)
-                <option value="{{ $departement->departement }}">{{ $departement->departement }}</option>
-            @endforeach
-        </select>
-        <input type="hidden" name="selected_departement" id="selected_departement">
-        <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">Submit</button>
-    </form>
-
+    <h1 class="text-2xl font-bold mb-4 text-center m-5">الوحدة/المصلحة</h1>
+    <div class="flex justify-center mx-auto my-16">
+        <form id="departementForm" action="{{ route('departement.show', ['departement' => '__departement__']) }}" method="GET" class="flex items-center">
+            <select name="departement" id="departementSelect" class="px-4 py-2 border border-gray-300 rounded-md mr-2 h-20 w-[450px]">
+                    <option value="">--SELECT AN OPTION--</option>
+                @foreach ($departements as $departement)
+                    <option value="{{ $departement->departement }}">{{ $departement->departement }}</option>
+                @endforeach
+            </select>
+            <input type="hidden" name="selected_departement" id="selected_departement">
+            <button type="submit" class="bg-blue-500 text-white px-4 py-2 text-2xl rounded-md hover:bg-blue-600 h-20 w-48">بحث</button>
+        </form>
+    </div>
     <script>
         document.getElementById('departementSelect').addEventListener('change', function() {
             var selectedDepartement = this.value;
