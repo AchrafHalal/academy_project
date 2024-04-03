c0c4973055acd4603f28be7d77700b600815efc4
Othmansalahi <ootmansalhi.24@gmail.com>
2024-04-03 11:51:37 +0000

    Refactor DepartementController to improve code readability and add new functionality

--- app/Http/Controllers/DepartementController.php
+++ app/Http/Controllers/DepartementController.php
@@ -5,9 +5,27 @@ namespace App\Http\Controllers;
 use App\Models\Departement;
 use Illuminate\Http\Request;
 
+
 class DepartementController extends Controller
 {
-    public function index() {
-        return view('Departement.index', ['departements' => Departement::all()]);
+            public function index()
+            {
+                $departements = Departement::select('departement')
+                    ->groupBy('departement')
+                    ->get();
+
+                return view('Departement.index', ['departements' => $departements]);
     }
+
+    public function show($departement)
+    {
+        $departements = Departement::where('departement', $departement)->get();
+        return view('Departement.show', ['departements' => $departements, 'departement' => $departement]);
+    }
+
+    public function show_name($name) {
+        $departements = Departement::where('name', $name)->get();
+        return view('Departement.show_name', ['departements' => $departements, 'name' => $name]);
+    }
+
 }
