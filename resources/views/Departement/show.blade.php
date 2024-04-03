6f3f92192673d4982ae78cfb62cf8ea7c23f7a20
Othmansalahi <ootmansalhi.24@gmail.com>
2024-04-03 14:19:03 +0000

    Refactor Departement show view to display data in a table

--- resources/views/Departement/show.blade.php
+++ resources/views/Departement/show.blade.php
@@ -1,15 +1,37 @@
 @extends('Departement.layout')
 
 @section('navbar')
-    <h1>Show Departement: {{ $departement }}</h1>
+    <div class="flex justify-between items-center flex-col">
+        <h1 class="text-2xl font-bold">{{ $departement }}</h1>
 
-    @if($departements->isEmpty())
-        <p>No data found for this departements.</p>
-    @else
-        <ul>
-            @foreach($departements as $dept)
-                <li><a href="{{route('departement.show_name', ['name' => $dept->name])}}"> {{ $dept->name }} - {{ $dept->job }}</a></li>
-            @endforeach
-        </ul>
-    @endif
+        @if($departements->isEmpty())
+            <p>No data found for this departements.</p>
+        @else
+            <table class="mt-4 w-full">
+                <thead>
+                    <tr>
+                        <th class="px-4 py-2">الاسم</th>
+                        <th class="px-4 py-2">المهمة</th>
+                        <th class="px-4 py-2">الاطلاع</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($departements as $dept)
+                        <tr>
+                            <td class="border px-4 py-2 text-center">{{ $dept->name }}</td>
+                            <td class="border px-4 py-2 text-center">{{ $dept->job }}</td>
+                            <td class="border px-4 py-2 text-center">
+                                <a href="{{ route('departement.show_name', ['name' => $dept->name]) }}" class="text-blue-500 hover:underline">
+                                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                        المعلومات
+                                    </button>
+                                </a>
+                            </td>
+                            
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @endif
+    </div>
 @endsection
