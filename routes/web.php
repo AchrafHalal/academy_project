5d88f9d20fe4cea582ae893e471f5d47cb0054e9
AchrafHalal <achraf.halal.ax@gmail.com>
2024-04-15 11:07:55 +0100

    Refactor navigation and add Arabic translations

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DepartementController;
 use App\Http\Controllers\EtablissmentController;
+use App\Http\Controllers\InspectionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,12 +30,16 @@ Route::get('/add-directive', function() {
 
 Route::get('/departement',[DepartementController::class , 'index'])->name('departement');
 
+Route::get('/departement/{departement}',[DepartementController::class , 'show'])->name('departement.show');
 
 Route::get('/etablissement',[EtablissmentController::class , 'index'])->name('etablissement');
 
 Route::get('/etablissement/{etablissement}',[EtablissmentController::class , 'show'])->name('etablissement.show');
 
-Route::get('/departement/{departement}',[DepartementController::class , 'show'])->name('departement.show');
+Route::get('/inspection',[InspectionController::class , 'index'])->name('inspection');
 
 
+Route::get('/inspection/{region}/{section}/{specialisation}', [InspectionController::class, 'show'])->name('inspection.show');
+
+Route::get('/inspection/{name}', [InspectionController::class, 'show_name'])->name('inspection.show_name');
 
