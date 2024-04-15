0eed81d8d8961b7e86dc7ab592a813581bd256a5
AchrafHalal <achraf.halal.ax@gmail.com>
2024-04-15 11:07:31 +0100

    Add InspectionController and Inspection model

---
+++ app/Http/Controllers/InspectionController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Inspection;
+use Illuminate\Http\Request;
+
+class InspectionController extends Controller
+{
+    public function index()
+    {
+        $inspections = Inspection::select('region', 'section', 'specialisation')
+                             ->groupBy('region', 'section', 'specialisation')
+                             ->distinct()
+                             ->get();
+
+        return view('inspection.index', ['inspections' => $inspections]);
+    }
+
+    public function show($region, $section, $specialisation)
+    {
+        $inspections = Inspection::where('region', $region)
+                             ->where('section', $section)
+                             ->where('specialisation', $specialisation)
+                             ->get();
+
+        return view('inspection.show', ['inspections' => $inspections, 'inspection' => $region]);
+    }
+
+    public function show_name($name) {
+        $inspections = Inspection::where('inspecteur_name', $name)->get();
+        return view('inspection.show_name', ['inspections' => $inspections, 'name' => $name]);
+    }
+}
