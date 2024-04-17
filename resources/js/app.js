0498bbf736e438c0b87d0596e28dec5ab24fd972
Othmansalahi <ootmansalhi.24@gmail.com>
2024-04-17 14:42:25 +0100

    Refactor CSS styles, update layout, and add toggle functionality in app.css, app.js, and layout.blade.php

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+
+let ul = document.querySelector('ul');
+let toggle = document.querySelector('#toggle');
+let div_ul = document.querySelector('#div-ul');
+
+toggle.addEventListener('click', () => {
+    ul.classList.toggle('hidden');
+    ul.classList.toggle('absolute');
+    div_ul.classList.toggle('bg-white');
+});
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,42 +6,49 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title','index')</title>
     @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
 
 </head>
 <body>
-    <nav class="navb">
-        <ul class="flex gap-4">
-            <li class="pg">
-                <a href="{{route('etablissement')}}" class="pages">
-                    البحث بالمؤسسة
+    <nav class="navb lg:w-full md:w-48 w-26 flex flex-col justify-center items-center gap-4">
+        <div class="flex justify-center items-center">
+            <img src="logo.gif" class="w-64" alt="logo">
+        </div>
+        <button id="toggle" class="bg-red-100 w-12 lg:hidden block" >menu</button>
+        <div class="w-full" id="div-ul">
+            <ul class="md:flex-row flex flex-col gap-4">
+                <li class="pg">
+                    <a href="{{route('etablissement')}}" class="pages">
+                        البحث بالمؤسسة
+                    </a>
+                    </li>
+                <li>
+                    <a href="{{route('departement')}}" class="pages">
+                        البحث بالمصلحة
+                    </a>
+                </li>
+                <li class="pg">
+                <a href="#" class="pages">
+                     المديريات الاقليمية
                 </a>
                 </li>
-            <li>
-                <a href="{{route('departement')}}" class="pages">
-                    البحث بالمصلحة
-                </a>
-            </li>
-            <li class="pg">
-            <a href="#" class="pages">
-                 المديريات الاقليمية
-            </a>
-            </li>
-            <li class="pg">
-                <a href="{{route('inspection')}}" class="pages">
-                    هيئة التفتيش 
-                </a>    
-            </li>
-            <li class="pg">
-                <a href="{{route('ambassadeur')}}" class="pages">
-                    التوجيه التربوي
-                </a>
-            </li>
-            <li class="pg text-center">
-                <a href="{{route('association')}}" class="pages">
-                    جمعيات المجتمع المدني
-                </a>
-            </li>
-        </ul>
+                <li class="pg">
+                    <a href="{{route('inspection')}}" class="pages">
+                        هيئة التفتيش 
+                    </a>    
+                </li>
+                <li class="pg">
+                    <a href="{{route('ambassadeur')}}" class="pages">
+                        التوجيه التربوي
+                    </a>
+                </li>
+                <li class="pg text-center">
+                    <a href="{{route('association')}}" class="pages">
+                        جمعيات المجتمع المدني
+                    </a>
+                </li>
+            </ul>
+        </div>
     </nav>
     @yield('navbar')
 </body>
