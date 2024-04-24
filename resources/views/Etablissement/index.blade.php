11ddd165d9cb9c382ef2759fc81751ba90ded37b
Othmansalahi <ootmansalhi.24@gmail.com>
2024-04-24 12:04:00 +0100

    Update Arabic translation for the select option in index.blade.php and add Font Awesome CSS link in layout.blade.php

--- resources/views/Etablissement/index.blade.php
+++ resources/views/Etablissement/index.blade.php
@@ -3,7 +3,7 @@
     <h1 class="text-2xl font-bold mb-4 text-center m-5">المؤسسات</h1>
     <div class="flex justify-center mx-auto my-16">
         <form id="etablissementForm" action="{{ route('etablissement.show', ['etablissement' => '__etablissement__']) }}" method="GET" class="flex items-center">
-            <select name="etablissement" id="etablissementSelect" class="px-4 py-2 border border-gray-300 rounded-md mr-2 h-20 w-[450px]">
+            <select name="etablissement" id="etablissementSelect" class="px-4 py-2 border border-gray-300 rounded-md mr-2 h-20 w-[450px]" required>
                     <option value="" class="text-center text-xl">--المؤسسات--</option>
                 @foreach ($etablissements as $etablissement)
                     <option value="{{ $etablissement->Institution_name }}">{{ $etablissement->Institution_name }}</option>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <title>@yield('title','index')</title>
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
@@ -14,7 +15,7 @@
         <div class="flex justify-center items-center">
             <img src="{{asset('logo.gif')}}" class="w-64" alt="logo">
         </div>
-        <button id="toggle" class="bg-red-100 w-12 lg:hidden block" >menu</button>
+        <button id="toggle" class="w-12 lg:hidden block" ><i class="fas fa-bars"></i></button>
         <div class="w-full" id="div-ul">
             <ul class="md:flex-row flex flex-col gap-4">
                 <li class="pg">
@@ -50,6 +51,12 @@
             </ul>
         </div>
     </nav>
+    <footer class="absolute bottom-0 w-full">
+        <div class="flex justify-center">
+            <p class="text-gray-500 text-sm">© 2024 All rights reserved DEVOWFS201</p>
+        </div>
+    </footer>
+    
     @yield('navbar')
 </body>
 </html>
